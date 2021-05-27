00513c496dae7349b2d9cf3b06a9959bf63a92b5
Peter <schmidt.multimedia@gmail.com>
2021-05-27 13:03:32 +0200

    JS: btn + EventListener

--- events.js
+++ events.js
@@ -4,4 +4,15 @@ function ausgabe(outputStr) {
     console.log(outputStr);
 }
 
+/** Btn */
+let btn;
+btn = document.getElementById("trigBtn");
+btn = document;
+ausgabe(btn);
 
+btn.addEventListener("click",test);
+
+
+function test() {
+    ausgabe("hi");
+}
